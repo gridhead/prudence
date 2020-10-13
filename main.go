5dba8c707030ee593ac4924491b15e014a3e313e
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2020-10-14 00:36:04 +0530

    Added construction and Darvo info
    Signed-off-by: t0xic0der <akashdeep.dhar@gmail.com>

--- main.go
+++ main.go
@@ -8,11 +8,15 @@ import (
 var cetscycl bool
 var orbvcycl bool
 var conclave bool
+var cnstruct bool
+var drvodeal bool
 
 func main() {
 	flag.BoolVar(&cetscycl, "cetscycl", false, "Know the state of Plains of Eidolon")
 	flag.BoolVar(&orbvcycl, "orbvcycl", false, "Know the state of Orb Vallis")
 	flag.BoolVar(&conclave, "conclave", false, "Know the details about Conclave")
+	flag.BoolVar(&cnstruct, "cnstruct", false, "Know the progress of construction")
+	flag.BoolVar(&drvodeal, "drvodeal", false, "Know the deals available from Darvo")
 	flag.Parse()
 	if cetscycl {
 		requestor.CetusCycle("pc")
@@ -20,5 +24,9 @@ func main() {
 		requestor.VallisCycle("pc")
 	} else if conclave {
 		requestor.ConclaveChallenges("pc")
+	} else if cnstruct {
+		requestor.ConstructionProgress("pc")
+	} else if drvodeal {
+		requestor.DailyDeals("pc")
 	}
 }
--- requestor/worldstate.go
+++ requestor/worldstate.go
@@ -89,3 +89,55 @@ func ConclaveChallenges(platform string) {
 		fmt.Println("The information could not be fetched")
 	}
 }
+
+func ConstructionProgress(platform string) {
+	resp, eror := http.Get("https://api.warframestat.us/" + platform + "/constructionProgress")
+	if eror == nil {
+		body, eror := ioutil.ReadAll(resp.Body)
+		if eror == nil {
+			var jsonobjc gjson.Result = gjson.Parse(string(body))
+			fmt.Println(colorformat.BackGrnBold(colorformat.ForeBlkBold("PRUDENCE/CONSTRUCTION")))
+			var textobjc string = colorformat.ForeGrnBold("Progress so far") + "\n" +
+				colorformat.ForeGrnRglr("Fomorian") + " ....... " + jsonobjc.Get("fomorianProgress").String() + "\n" +
+				colorformat.ForeGrnRglr("Razorback") + " ...... " + jsonobjc.Get("razorbackProgress").String() + "\n"
+			fmt.Printf(textobjc)
+			defer resp.Body.Close()
+		} else {
+			fmt.Println(colorformat.BackRedBold(colorformat.ForeBlkBold("PRUDENCE/WARNING")))
+			fmt.Println("The information could not be fetched")
+		}
+	} else {
+		fmt.Println(colorformat.BackRedBold(colorformat.ForeBlkBold("PRUDENCE/WARNING")))
+		fmt.Println("The information could not be fetched")
+	}
+}
+
+func DailyDeals(platform string) {
+	resp, eror := http.Get("https://api.warframestat.us/" + platform + "/dailyDeals")
+	if eror == nil {
+		body, eror := ioutil.ReadAll(resp.Body)
+		if eror == nil {
+			var jsonobjc gjson.Result = gjson.Parse(string(body))
+			fmt.Println(colorformat.BackGrnBold(colorformat.ForeBlkBold("PRUDENCE/DEALS")))
+			jsonobjc.ForEach(func(keyd, valu gjson.Result) bool {
+				singchal := gjson.Parse(valu.String())
+				var textobjc string = colorformat.ForeGrnBold(singchal.Get("item").String()) + "\n" +
+					singchal.Get("salePrice").String() + "P (Was " + singchal.Get("originalPrice").String() + "P) @ " + singchal.Get("discount").String() + " percent discount" + "\n" +
+					colorformat.ForeGrnRglr("ID") + " ............. " + singchal.Get("id").String() + "\n" +
+					colorformat.ForeGrnRglr("Expiry") + " ......... " + singchal.Get("expiry").String() + "\n" +
+					colorformat.ForeGrnRglr("Activation") + " ..... " + singchal.Get("activation").String() + "\n" +
+					colorformat.ForeGrnRglr("Sold") + " ........... " + singchal.Get("sold").String() + " of " + singchal.Get("total").String() + " items \n" +
+					colorformat.ForeGrnRglr("Estimated time") + " . " + singchal.Get("eta").String() + "\n" + "\n"
+				fmt.Printf(textobjc)
+				return true
+			})
+			defer resp.Body.Close()
+		} else {
+			fmt.Println(colorformat.BackRedBold(colorformat.ForeBlkBold("PRUDENCE/WARNING")))
+			fmt.Println("The information could not be fetched")
+		}
+	} else {
+		fmt.Println(colorformat.BackRedBold(colorformat.ForeBlkBold("PRUDENCE/WARNING")))
+		fmt.Println("The information could not be fetched")
+	}
+}
