64e5130c602e084527b4357c079e9c7fce0bd1c7
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2020-10-13 23:56:54 +0530

    Added flags about CLI options
    Signed-off-by: t0xic0der <akashdeep.dhar@gmail.com>

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-	"./colorformat"
-	"fmt"
+	"./requestor"
+	"flag"
 )
 
+var cetscycl bool
+var orbvcycl bool
+var conclave bool
+
 func main() {
-	fmt.Println(colorformat.ForeRedRglr("Hello"))
+	flag.BoolVar(&cetscycl, "cetscycl", false, "Know the state of Plains of Eidolon")
+	flag.BoolVar(&orbvcycl, "orbvcycl", false, "Know the state of Orb Vallis")
+	flag.BoolVar(&conclave, "conclave", false, "Know the details about Conclave")
+	flag.Parse()
+	if cetscycl {
+		requestor.CetusCycle("pc")
+	} else if orbvcycl {
+		requestor.VallisCycle("pc")
+	} else if conclave {
+		requestor.ConclaveChallenges("pc")
+	}
 }
